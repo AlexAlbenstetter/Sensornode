ecb517a7d7b46186a48692508aa6fa6dab96295b
alexalbi92 <alexander.albenstetter@hotmail.de>
2022-12-09 20:20:57 +0100

    Added reading of Bosch BME280 sensor. Added creationTimestamp to information that is written to csv

--- DataCollector.py
+++ DataCollector.py
@@ -1,11 +1,25 @@
-import serial
-import logging
-import string
-import time
-import datetime
-import io
-import pynmea2
-import csv
+try:
+    import serial
+    import logging
+    import string
+    import time
+    import datetime
+    import io
+    import pynmea2
+    import csv
+    import math
+
+except ImportError as e:
+    print('Import Error: {}'.format(e))
+    logging.error('Import Error: {}'.format(e))
+
+# Imports for BME280 
+try:
+    from smbus2 import SMBus
+except ImportError as e:
+    from smbus import SMBus
+from bme280 import BME280
+
 
 
 class DataStorage:
@@ -21,7 +35,7 @@ class DataStorage:
             filename= f'{day}{month}{year}_{time}_Measurements.csv'
             file = open(filename, "w", encoding='UTF8')
             writer = csv.writer(file)
-            header = ['Longitude', 'Latitude', 'Temperature', 'Humidity', 'Pressure','PM2.5','PM10']
+            header = ['Latitude','Longitude', 'Temperature', 'Humidity', 'Pressure','PM2.5','PM10']
             writer.writerow(header)
             return writer
 
@@ -29,10 +43,35 @@ class DataStorage:
             print('CSV file error: {}'.format(e))
             logging.error('CSV file error: {}'.format(e))
 
+"""
+Nicht mehr notwendig, aber evtl. gut um den Umgang mit self und init method abzugucken
+class Sensor_BME280:
+
+    def __init__(self):
+
+   
+    
+    def getData(self):
+        temperature = self.sensor.get_temperature()
+        pressure = self.sensor.get_pressure()
+        humidity = self.sensor.get_humidity()
+        sensordata = [temperature, pressure, humidity]
+        return sensordata
+
+    def printData(self):
+        temperature = self.sensor.get_temperature()
+        pressure = self.sensor.get_pressure()
+        humidity = self.sensor.get_humidity()
+        print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperature, pressure, humidity))
+"""
+
+class Sensor:
+    def SPS30():
+        pass
 
 class GPS:
     #Function getGPS() collects GPS data from GPS receiver
-    def getGPS(writer):
+    def getGPS(self,writer,bme280Sensor):
         #define serial connection with UART
         port="/dev/ttyAMA0"
         ser=serial.Serial(port, baudrate=9600, timeout=5.0)
@@ -46,11 +85,24 @@ class GPS:
                 substring =line[0:6]
                 if substring == startparams:
                     newmsg=pynmea2.parse(line)
+                    #GPS Data:
                     lat=newmsg.latitude
                     long=newmsg.longitude
-                    gps = "Latitude" + str(lat) + "and Longitude=" + str(long)
-                    print(gps)
-                    dataline=[lat,long]
+
+                    #Bosch BME280 Data:
+                    temp = round(bme280Sensor.get_temperature(),4)
+                    hum = round(bme280Sensor.get_humidity(),4)
+                    pres = round(bme280Sensor.get_pressure(),4)
+
+                    #Create Timestamp:
+                    creationTimestamp = datetime.datetime.now()
+                    #hier noch pressure und humidity einfügen
+                    dataString = "Latitude: "+str(lat)+" | Longitude: " +str(long)+ " | Temperature: " +str(temp)+" | Humidity: "+str(hum)+" | Pressure: "+str(pres)
+                    print(dataString)
+        
+                    #Prepare array for 'write to file'
+                    dataline=[creationTimestamp,lat,long,temp,hum,pres]
+                    #Append new data row to file
                     writer.writerow(dataline)
 
             except pynmea2.ParseError as e:
@@ -61,12 +113,7 @@ class GPS:
                 print('Serial UART error: {}'.format(e))
                 logging.error(e)
                 break
-class Sensor:
-    def SPS30():
-        print('test SPS30')
 
-    def BMP280():
-        print('test BMP280')
 
 def main():
 
@@ -78,7 +125,16 @@ def main():
     print('Setup csv file for data storage')
     file= DataStorage.setupFile()
     print('CSV file setup success')
-    GPS.getGPS(file)
+
+    #initialize BME280 sensor
+    #Select I2C Bus to which the device/sensor is connected
+    bus = SMBus(1)
+    bme280Sensor = BME280(i2c_dev=bus)
+    print('initialize Bosch Sensor BMP280')  
+
+    #create object called gps of GPS class
+    gps = GPS()
+    gps.getGPS(file,bme280Sensor)
 
 if __name__ == "__main__":
     main()
